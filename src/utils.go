ea3a6450fb3a2cc428ce3e771057532658ac0357
whyhilde <whyhilde@gmail.com>
2026-02-05 21:00:29 +0500

    feat: add byte formatting utility for progress bar

---
+++ src/utils.go
@@ -0,0 +1,18 @@
+package src
+
+import "fmt"
+
+func formatBytes(bytes int64) string {
+	const unit = 1024
+	if bytes < unit {
+		return fmt.Sprintf("%d B", bytes)
+	}
+
+	div, exp := int64(unit), 0
+	for n := bytes / unit; n >= unit; n /= unit {
+		div *= unit
+		exp ++
+	}
+
+	return fmt.Sprintf("%.1f %cB", float64(bytes)/float64(div), "KMGTPE"[exp])
+}
